f2d77ff396ac6dc937d0e7e2836bcf0d26f04085
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-01-15 23:37:55 -0200

    update  Brazilian Portuguese translation

--- language/pt_br/common.php
+++ language/pt_br/common.php
@@ -1,8 +1,8 @@
 <?php
 /**
 *
-* prime_post_revisions [Português do Brasil]
-*
+* Prime Post Revisions extension for the phpBB Forum Software package.
+* Brazilian Portuguese translation by eunaumtenhoid [2019][ver 1.0.0-beta5] (https://github.com/phpBBTraducoes)
 * @copyright (c) 2018 Ken F. Innes IV <https://www.absoluteanime.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
@@ -21,40 +21,40 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
-// ATENÇÃO DESENVOLVEDORES
+// DEVELOPERS PLEASE NOTE
 //
-// Todos os arquivos devem usar linguagem UTF-8 como a sua codificação e os arquivos não devem conter um BOM.
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
 //
-// Espaços, podem conter agora, informação de ordem, por exemplo, ao invés vez de
-//"Página %s de %s' você poderá (e deveria) escrever 'Page %1$s of %2$s', isso permite
-// a tradutores re-ordenar a saída de dados, enquanto garante que continue correta
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
 //
-// Você não precisa disso onde espaços únicos são usados, por exemplo, 'Message %d' está bom
-// igualmente, onde uma string contem apenas dois espaços que são usados para ajustar o texto
-// numa url você também não precisa especificar uma ordem, por exemplo, 'Click %sHERE%s' está bom
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 //
-// Alguns carácteres que você pode querer copiar&colar:
-// ’ » “ ” …
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
 //
 
 $lang = array_merge($lang, array(
-	// Observando a mensagem
-	'PRIMEPOSTREVISIONS_VIEW'				=> 'Observar histórico da mensagem.',	// Texto para o link de observar o histórico de revisões
+	// Viewing posts
+	'PRIMEPOSTREVISIONS_VIEW'				=> 'Visualizar histórico do post.',	// Text for the link to view the revision history
 
-	// Observando revisões
-	'PRIMEPOSTREVISIONS_VIEWING'			=> 'Observando histórico da mensagem.',
-	'PRIMEPOSTREVISIONS_VIEWING_EXPLAIN'	=> 'Esta página mostra todas as versões da mensagem, começando da mais atual.',
-	'PRIMEPOSTREVISIONS_TITLE'				=> 'Observando histórico da mensagem',
-	'PRIMEPOSTREVISIONS_FIRST'				=> 'Mensagem original',
-	'PRIMEPOSTREVISIONS_FINAL'				=> 'Mensagem atual',
+	// Viewing revisions
+	'PRIMEPOSTREVISIONS_VIEWING'			=> 'Visualizando histórico do post.',
+	'PRIMEPOSTREVISIONS_VIEWING_EXPLAIN'	=> 'Esta página mostra todas as versões do post, começando da mais atual.',
+	'PRIMEPOSTREVISIONS_TITLE'				=> 'Visualizando histórico do post',
+	'PRIMEPOSTREVISIONS_FIRST'				=> 'Post original',
+	'PRIMEPOSTREVISIONS_FINAL'				=> 'Post atual',
 	'PRIMEPOSTREVISIONS_COUNT'				=> 'revisão %d',
 	#'PRIMEPOSTREVISIONS_INFO'				=> 'Editado por %1$s em %2$s.',
 	'PRIMEPOSTREVISIONS_EDIT_BY'			=> 'Editado por',
 	'PRIMEPOSTREVISIONS_NO_SUBJECT'			=> '[sem assunto]',
 	'PRIMEPOSTREVISIONS_COMPARE'			=> 'Compare',
-	'PRIMEPOSTREVISIONS_VIEW_DENIED'		=> 'Você não tem as permissões necessárias para visualizar as revisões de mensagem.',
+	'PRIMEPOSTREVISIONS_VIEW_DENIED'		=> 'Você não tem as permissões necessárias para visualizar as revisões do post.',
 
-	// Apagar uma revisão
+	// Delete a revision
 	'PRIMEPOSTREVISIONS_DELETE'				=> 'Excluir Revisão',
 	'PRIMEPOSTREVISIONS_DELETE_CONFIRM'		=> 'Quer realmente apagar esta revisão?',
 	'PRIMEPOSTREVISIONS_DELETE_DENIED'		=> 'Você não tem as permissões necessárias para remover esta revisão.',
@@ -62,7 +62,7 @@ $lang = array_merge($lang, array(
 	'PRIMEPOSTREVISIONS_DELETE_SUCCESS'		=> 'Revisão removida com sucesso.',
 	'PRIMEPOSTREVISIONS_DELETE_INVALID'		=> 'Nenhuma revisão selecionada para remoção.',
 
-	// Apagar todas revisões
+	// Delete all revisions
 	'PRIMEPOSTREVISIONS_DELETES'			=> 'Excluir revisões selecionadas',
 	'PRIMEPOSTREVISIONS_DELETES_CONFIRM'	=> 'Quer realmente apagar estas revisões?',
 	'PRIMEPOSTREVISIONS_DELETES_DENIED'		=> 'Você não tem as permissões necessárias para remover estas revisões.',
@@ -75,5 +75,7 @@ $lang = array_merge($lang, array(
 	'PRIMEPOSTREVISIONS_RESTORE_CONFIRM'	=> 'Quer realmente restaurar esta revisão?',
 	'PRIMEPOSTREVISIONS_RESTORE_DENIED'		=> 'Você não tem as permissões necessárias para restaurar esta revisão.',
 	'PRIMEPOSTREVISIONS_RESTORE_FAILED'		=> 'Ocorreu um erro ao tentar restaurar a revisão.',
-	'PRIMEPOSTREVISIONS_RESTORE_SUCCESS'	=> 'A mensagem foi restaurada com sucesso.',
+	'PRIMEPOSTREVISIONS_RESTORE_SUCCESS'	=> 'O post foi restaurado com sucesso.',
+	'PRIMEPOSTREVISIONS_RESTORE_INVALID'	=> 'Nenhuma revisão posterior foi selecionada para restauração.',
+
 ));
---
+++ language/pt_br/info_acp_main.php
@@ -0,0 +1,48 @@
+<?php
+/**
+*
+* Prime Post Revisions extension for the phpBB Forum Software package.
+* Brazilian Portuguese translation by eunaumtenhoid [2019][ver 1.0.0-beta5] (https://github.com/phpBBTraducoes)
+* @copyright (c) 2018 Ken F. Innes IV <https://www.absoluteanime.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'ACP_PRIMEPOSTREVISIONS_ENABLE'				=> 'Armazenar revisões de post',
+	'ACP_PRIMEPOSTREVISIONS_ENABLE_EXPLAIN'		=> 'Mantenha um histórico de revisão de todas as edições de posts.',
+
+	'ACP_PRIMEPOSTREVISIONS_AUTOPRUNE'			=> 'Auto-limpeza revisões',
+	'ACP_PRIMEPOSTREVISIONS_AUTOPRUNE_EXPLAIN'	=> 'Excluir automaticamente as revisões de posts anteriores ao número de dias especificado. Defina como 0 para desativar.',
+
+	'LOG_PRIMEPOSTREVISIONS_AUTOPRUNE'			=> '<strong>Auto-limpou revisões de post</strong><br />» Revisões excluidas: %d',
+));
