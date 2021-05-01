09443cb4dddf57b37a79521008afd73341c1a6bf
Adam Sprada <adam@sprada.pl>
2021-05-01 21:47:15 +0200

    Update Polish language files.

--- language/pl/info_acp_main.php
+++ language/pl/info_acp_main.php
@@ -39,31 +39,31 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, [
 	'ACP_PRIMEPOSTREVISIONS_TITLE'						=> 'Prime Post Revisions',
-	'ACP_PRIMEPOSTREVISIONS_SETTINGS'					=> 'Extension Settings',
-	'ACP_PRIMEPOSTREVISIONS_BASIC_SETTINGS'				=> 'General Settings',
+	'ACP_PRIMEPOSTREVISIONS_SETTINGS'					=> 'Ustawienia rozszerzenia',
+	'ACP_PRIMEPOSTREVISIONS_BASIC_SETTINGS'				=> 'Ustawienia ogólne',
 
-	'ACP_PRIMEPOSTREVISIONS_ENABLE_GENERAL'				=> 'Enable Post Revisions',
-	'ACP_PRIMEPOSTREVISIONS_ENABLE_GENERAL_EXPLAIN'		=> 'Enables the ability to store post revisions. Storing revisions must be set on a per-forum basis. Disabling this will not delete existing post revisions, it only prevents new revisions from being stored.',
+	'ACP_PRIMEPOSTREVISIONS_ENABLE_GENERAL'				=> 'Włącz wersje postów',
+	'ACP_PRIMEPOSTREVISIONS_ENABLE_GENERAL_EXPLAIN'		=> 'Włącza możliwość przechowywanie wersji postów. Przechowywanie wersji musi zostać ustawione dla każdego forum z osobna. Wyłączenie tej opcji nie spowoduje usunięcia istniejących wersji postów, a jedynie zapobiega zapisywaniu nowych wersji.',
 
-	'ACP_PRIMEPOSTREVISIONS_ENABLE_AUTOPRUNE'			=> 'Enable Auto-prune',
-	'ACP_PRIMEPOSTREVISIONS_ENABLE_AUTOPRUNE_EXPLAIN'	=> 'Enables the ability to auto-prune revisions. Pruning frequency must be set on a per-forum basis.',
+	'ACP_PRIMEPOSTREVISIONS_ENABLE_AUTOPRUNE'			=> 'Włącz automatyczne usuwanie',
+	'ACP_PRIMEPOSTREVISIONS_ENABLE_AUTOPRUNE_EXPLAIN'	=> 'Włącza możliwość automatycznego usuwania wersji. Częstotliwość usuwania należy ustawić dla każdego forum z osobna.',
 
-	'ACP_PRIMEPOSTREVISIONS_FORUM_SETTINGS'				=> 'Forum Settings',
-	'ACP_PRIMEPOSTREVISIONS_FORUM_SETTINGS_EXPLAIN'		=> 'Keep a revision history of all posts edits. Auto-prune will automatically delete revisions older than the given number of days. Set to 0 to disable.',
-	'ACP_PRIMEPOSTREVISIONS_FORUM_SETTINGS_AUTOPRUNE'	=> 'Auto-prune',
-	'ACP_PRIMEPOSTREVISIONS_FORUM_NAME'					=> 'Forum Name',
-	'ACP_PRIMEPOSTREVISIONS_FORUM_ENABLE'				=> 'Enable',
+	'ACP_PRIMEPOSTREVISIONS_FORUM_SETTINGS'				=> 'Ustawienia forum',
+	'ACP_PRIMEPOSTREVISIONS_FORUM_SETTINGS_EXPLAIN'		=> 'Zachowaj historię wersji wszystkich zmian w postach. Funkcjonalność automatycznego czyszczenie automatycznie usunie wersje starsze niż podana liczba dni. Ustaw wartość 0 aby wyłączyć.',
+	'ACP_PRIMEPOSTREVISIONS_FORUM_SETTINGS_AUTOPRUNE'	=> 'Automatyczne usuwanie',
+	'ACP_PRIMEPOSTREVISIONS_FORUM_NAME'					=> 'Nazwa forum',
+	'ACP_PRIMEPOSTREVISIONS_FORUM_ENABLE'				=> 'Włącz',
 
-	'ACP_PRIMEPOSTREVISIONS_SETTINGS_SAVED'				=> 'Prime Post Revisions settings have been saved successfully!',
-	'ACP_PRIMEPOSTREVISIONS_SETTINGS_LOG'				=> '<strong>Altered Prime Post Revisions settings</strong>',
+	'ACP_PRIMEPOSTREVISIONS_SETTINGS_SAVED'				=> 'Ustawienia Prime Post Revisions zostały zapisane pomyślnie!',
+	'ACP_PRIMEPOSTREVISIONS_SETTINGS_LOG'				=> '<strong>Ustawienia Prime Post Revisions zostały zmienione</strong>',
 
 	// Forum Settings
-	'ACP_PRIMEPOSTREVISIONS_ENABLE'						=> 'Store post revisions',
-	'ACP_PRIMEPOSTREVISIONS_ENABLE_EXPLAIN'				=> 'Keep a revision history of all posts edits.',
+	'ACP_PRIMEPOSTREVISIONS_ENABLE'						=> 'Zachowuj wersje postów',
+	'ACP_PRIMEPOSTREVISIONS_ENABLE_EXPLAIN'				=> 'Zachowaj historię wersji wszystkich zmian w postach.',
 
-	'ACP_PRIMEPOSTREVISIONS_AUTOPRUNE'					=> 'Auto-prune revisions',
-	'ACP_PRIMEPOSTREVISIONS_AUTOPRUNE_EXPLAIN'			=> 'Automatically delete post revisions older than the given number of days. Set to 0 to disable.',
+	'ACP_PRIMEPOSTREVISIONS_AUTOPRUNE'					=> 'Automatyczne usuwanie wersji',
+	'ACP_PRIMEPOSTREVISIONS_AUTOPRUNE_EXPLAIN'			=> 'Wersje starsze niż zdefiniowana wartość będą automatycznie usuwane. Ustaw wartość 0 aby wyłączyć.',
 
 	// Cron Log
-	'LOG_PRIMEPOSTREVISIONS_AUTOPRUNE'					=> '<strong>Auto-pruned post revisions</strong><br />» Revisions deleted: %d » %s',
+	'LOG_PRIMEPOSTREVISIONS_AUTOPRUNE'					=> '<strong>Automatycznie usunięte wersje postów</strong><br />» Usunięte wersje: %d » %s',
 ]);
