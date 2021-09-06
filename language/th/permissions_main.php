e1e913cc4a4a8d854020aa1c83846be8c747335c
achia70 <65407363+achia70@users.noreply.github.com>
2021-09-06 13:53:48 +0700

    Create permissions_main.php

---
+++ language/th/permissions_main.php
@@ -0,0 +1,47 @@
+<?php
+/**
+*
+* Prime Post Revisions extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2018 Ken F. Innes IV <https://www.absoluteanime.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, [
+	'ACL_M_PRIMEPOSTREV_VIEW'		=> 'สามารถดูการแก้ไขโพสต์',
+	'ACL_M_PRIMEPOSTREV_DELETE'		=> 'สามารถลบการแก้ไขโพสต์',
+	'ACL_M_PRIMEPOSTREV_RESTORE'	=> 'สามารถเรียกคืนการแก้ไขหลังการแก้ไข',
+	'ACL_F_PRIMEPOSTREV_VIEW'		=> 'สามารถดูการแก้ไขโพสต์ของตัวเองได้',
+	'ACL_F_PRIMEPOSTREV_DELETE'		=> 'สามารถลบการแก้ไขของโพสต์เองได้',
+	'ACL_F_PRIMEPOSTREV_RESTORE'	=> 'สามารถกู้คืนการแก้ไขของโพสต์เองได้',
+]);
